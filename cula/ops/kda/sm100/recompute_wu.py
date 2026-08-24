4406209ec6b80dc4bf14a304fb0c433ed7cd52d2
shuyan.ycf <shuyan.ycf@antgroup.com>
2026-08-24 13:09:37 +0800

    revert: respect SM100 register floor
    Signed-off-by: shuyan.ycf <shuyan.ycf@antgroup.com>

--- cula/ops/kda/sm100/recompute_wu.py
+++ cula/ops/kda/sm100/recompute_wu.py
@@ -109,8 +109,8 @@ class KDARecomputeWU:
         #   Regs: 128*216 + 128*32 = 31,744 registers/CTA; two CTAs fit.
         #   SMEM: ~74KB/CTA × 2 = 148KB < 228KB ✓
         self.min_occupancy = 2
-        self.num_regs_cuda = 240
-        self.num_regs_others = 16
+        self.num_regs_cuda = 232
+        self.num_regs_others = 24
         self.a_stage = 1
         self.kgk_stage = 1
         self.v_tma_stage = 1
